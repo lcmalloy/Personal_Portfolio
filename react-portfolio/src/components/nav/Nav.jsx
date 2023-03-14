44947b26210eba0f7d51282858f9cb21c1ed2a2a
Lawrence Malloy <lcmalloy@gmail.com>
2023-03-13 20:51:10 -0700

    added react icons to nav bar

--- react-portfolio/src/components/nav/Nav.jsx
+++ react-portfolio/src/components/nav/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+
+import { IoHomeOutline } from 'react-icons/io5'
+import { AiOutlineUser } from 'react-icons/ai'
+import { BiBookContent, BiCoffeeTogo } from 'react-icons/bi'
+import { RiComputerLine } from 'react-icons/ri'
 import './nav.scss'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><IoHomeOutline/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBookContent/></a>
+      <a href="#portfolio"><RiComputerLine/></a>
+      <a href="#contact"><BiCoffeeTogo/></a>
+
+    </nav>
   )
 }
 
