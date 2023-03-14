0299e31f08d1029cf47655a00d07acf4b93ef173
Lawrence Malloy <lcmalloy@gmail.com>
2023-03-14 16:45:06 -0700

    added contact feature created form to send email connected form to emailjs to handle email network functionality.

--- react-portfolio/src/components/contact/Contact.jsx
+++ react-portfolio/src/components/contact/Contact.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+
+import {AiOutlinePhone, AiOutlineMail} from 'react-icons/ai'
 import './contact.scss'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_he4cpc2', 'template_owfpyf1', form.current, 'c3rdr51CV178oCjR8')
+    .then((result) => {
+      console.log(result.text);
+    }, (error) => {
+      console.log(error.text);
+    });
+    e.target.reset();
+  }
+
+
   return (
-    <section id="contact"></section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiOutlineMail className="contact__option-icon"/>
+            <h4>Email</h4>
+            <h5>Lcmalloy30@gmail.com</h5>
+            <a href="mailto:lcmalloy30@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <AiOutlinePhone className="contact__option-icon"/>
+            <h4>Phone Number</h4>
+            <h5>1-925-336-9244</h5>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Full Name" required/>
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+          <button type="submit" className="btn btn-primary">Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- react-portfolio/src/components/portfolio/Portfolio.jsx
+++ react-portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+
+import proj1 from '../../assets/littleLemon.png'
 import './portfolio.scss'
 
+const projects = [
+  {
+    id: 1,
+    image: proj1,
+    title: 'Little Lemon',
+    github: "https://github.com/",
+    demo: ""
+  }
+]
+
 const Portfolio = () => {
+
+  const project = projects
   return (
-    <section id="portfolio"></section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+      {projects.map((project) =>  (
+        <article className="portfolio__item" key={project.id}>
+          <div className="portfolio__item-image">
+            <img src={project.image} alt={project.title} />
+          </div>
+          <h3>{project.title}</h3>
+          <div className="portfolio__item-CTA">
+            <a href={project.github} className='btn' target="_blank" rel="noreferrer">Github</a>
+            <a href={project.demo} className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
+          </div>
+        </article>
+      ))}
+      </div>
+    </section>
   )
 }
 
