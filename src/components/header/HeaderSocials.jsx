5c522c9a65cfc2f977dc663fcc7f80c8b1fa0b83
Lawrence Malloy <lcmalloy@gmail.com>
2023-03-16 14:00:15 -0700

    fixed eslint errors

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,8 +4,8 @@ import { FaLinkedin, FaGithub } from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://www.linkedin.com/in/lawrence-malloy/" target="_blank"><FaLinkedin/></a>
-      <a href="https://github.com/lcmalloy" target="_blank"><FaGithub/></a>
+      <a href="https://www.linkedin.com/in/lawrence-malloy/" target="_blank" rel="noreferrer"><FaLinkedin/></a>
+      <a href="https://github.com/lcmalloy" target="_blank" rel="noreferrer"><FaGithub/></a>
     </div>
   )
 }
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -10,7 +10,7 @@ const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><IoHomeOutline/></a>
+      <a href="/#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><IoHomeOutline/></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBookContent/></a>
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><RiComputerLine/></a>
